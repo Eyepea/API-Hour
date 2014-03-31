21da289c2572b443c167dfb8c2a9804cdca6098a
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-03-31 17:08:26 +0300

    Add setup.py and version

--- aiorest/__init__.py
+++ aiorest/__init__.py
@@ -13,8 +13,45 @@ from types import MethodType
 from datetime import datetime, timedelta
 from urllib.parse import urlsplit, parse_qsl
 
+from collections import namedtuple
+import sys
+
 from .multidict import MultiDict
 from .util import _ReadonlyDict
+
+
+
+__version__ = '0.0.1a0'
+
+version = __version__ + ' , Python ' + sys.version
+
+
+VersionInfo = namedtuple('VersionInfo',
+                         'major minor micro releaselevel serial')
+
+
+def _parse_version(ver):
+    RE = (r'^(?P<major>\d+)\.(?P<minor>\d+)\.'
+          '(?P<micro>\d+)((?P<releaselevel>[a-z]+)(?P<serial>\d+)?)?$')
+    match = re.match(RE, ver)
+    try:
+        major = int(match.group('major'))
+        minor = int(match.group('minor'))
+        micro = int(match.group('micro'))
+        levels = {'rc': 'candidate',
+                  'a': 'alpha',
+                  'b': 'beta',
+                  None: 'final'}
+        releaselevel = levels[match.group('releaselevel')]
+        serial = int(match.group('serial')) if match.group('serial') else 0
+        return VersionInfo(major, minor, micro, releaselevel, serial)
+    except Exception:
+        raise ImportError("Invalid package version {}".format(ver))
+
+
+version_info = _parse_version(__version__)
+
+
 
 
 Entry = collections.namedtuple('Entry', 'regex method handler use_request')
---
+++ setup.py
@@ -0,0 +1,62 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+install_requires = ['aiohttp>=0.4']
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("aiorest doesn't suppport Python earllier than 3.3")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__), 'aiozmq', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in aiozmq/__init__.py')
+
+
+classifiers=[
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Operating System :: OS Independent',
+    'Environment :: Web Environment',
+]
+
+
+setup(name='aiozmq',
+      version='0.0.1',
+      description=('Support REST calls for asyncio+aiohttp.'),
+      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+      classifiers=classifiers,
+      platforms=['OS Independent'],
+      author='Andrew Svetlov',
+      author_email='andrew.svetlov@gmail.com',
+      url='http://aiorest.readthedocs.org',
+      download_url='https://pypi.python.org/pypi/aiorest',
+      license='BSD',
+      packages=find_packages(),
+      install_requires = install_requires,
+      #tests_require = tests_require,
+      #test_suite = 'nose.collector',
+      provides = ['aiorest'],
+      requires = ['aiohttp'],
+      include_package_data = True)
