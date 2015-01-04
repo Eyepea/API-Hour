a737ed30ea3a5db13736be9d52537824a7666a35
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-01-04 13:07:11 +0100

    Fix ctrl-C exception in worker, thanks to aiohttp team

--- api_hour/worker.py
+++ api_hour/worker.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -
-#
-# This file is part of API-Hour, forked from gunicorn.workers._gaiohttp.py released under the MIT license.
-# See the NOTICE for more information.
+# This file is part of API-Hour, forked from aiohttp/worker.py.
+
+__all__ = ['Worker']
 
 import asyncio
 import os
+import signal
+import sys
 import gunicorn.workers.base as base
 
 
@@ -13,8 +14,9 @@ class Worker(base.Worker):
     def __init__(self, *args, **kw):  # pragma: no cover
         super().__init__(*args, **kw)
 
-        self.servers = []
-        self.connections = {}
+        self.servers = {}
+        self.exit_code = 0
+        self.container = None
 
     def init_process(self):
         # create new event_loop after fork
@@ -31,90 +33,79 @@ class Worker(base.Worker):
         try:
             self.loop.run_until_complete(self._runner)
         finally:
-            # self._runner.cancel() # @todo: Fix ctrl-c stack trace pending task remaining
             self.loop.close()
 
-    def wrap_protocol(self, proto):
-        proto.connection_made = _wrp(
-            proto, proto.connection_made, self.connections)
-        proto.connection_lost = _wrp(
-            proto, proto.connection_lost, self.connections, False)
-        return proto
+        sys.exit(self.exit_code)
 
     @asyncio.coroutine
     def close(self):
-        try:
-            if hasattr(self.wsgi, 'close'):
-                yield from self.wsgi.close()
-        except:
-            self.log.exception('Process shutdown exception')
+        if self.servers:
+            servers = self.servers
+            self.servers = None
+
+            # stop accepting connections
+            for server, handler in servers.items():
+                self.log.info("Stopping server: %s, connections: %s",
+                              self.pid, len(handler.connections))
+                server.close()
+
+            # stop alive connections
+            tasks = [
+                handler.finish_connections(
+                    timeout=self.cfg.graceful_timeout / 100 * 80)
+                for handler in servers.values()]
+            yield from asyncio.wait(tasks, loop=self.loop)
+
+            # stop container
+            yield from self.container.stop()
 
     @asyncio.coroutine
     def _run(self):
-        api_hour_app = self.app.callable(config=self.app.config, loop=self.loop)
-        handlers = api_hour_app.make_servers()
+        self.container = self.app.callable(config=self.app.config, loop=self.loop)
+        handlers = self.container.make_servers()
         for i, sock in enumerate(self.sockets):
             if len(handlers) == 1:
                 handler = handlers[0]
             else:
                 handler = handlers[i]
 
-            self.servers.append(
-                (yield from self.loop.create_server(handler, sock=sock.sock)))
-        yield from api_hour_app.start()
+            srv = yield from self.loop.create_server(handler, sock=sock.sock)
+            self.servers[srv] = handler
+        yield from self.container.start()
 
         # If our parent changed then we shut down.
         pid = os.getpid()
         try:
-            while self.alive or self.connections:
+            while self.alive:
                 self.notify()
 
-                if (self.alive and
-                        pid == os.getpid() and self.ppid != os.getppid()):
-                    self.log.info("Parent changed, shutting down: %s", self)
+                if pid == os.getpid() and self.ppid != os.getppid():
                     self.alive = False
-
-                # stop accepting requests
-                if not self.alive:
-                    if self.servers:
-                        self.log.info(
-                            "Stopping server: %s, connections: %s",
-                            pid, len(self.connections))
-                        for server in self.servers:
-                            server.close()
-                        self.servers.clear()
-
-                    # prepare connections for closing
-                    for conn in self.connections.values():
-                        if hasattr(conn, 'closing'):
-                            conn.closing()
-
-                yield from asyncio.sleep(1.0, loop=self.loop)
+                    self.log.info("Parent changed, shutting down: %s", self)
+                else:
+                    yield from asyncio.sleep(1.0, loop=self.loop)
         except (Exception, BaseException, GeneratorExit, KeyboardInterrupt):
             pass
 
-        yield from api_hour_app.stop()
-        if self.servers:
-            for server in self.servers:
-                server.close()
-
         yield from self.close()
 
+    def init_signal(self):
+        # init new signaling
+        self.loop.add_signal_handler(signal.SIGQUIT, self.handle_quit)
+        self.loop.add_signal_handler(signal.SIGTERM, self.handle_exit)
+        self.loop.add_signal_handler(signal.SIGINT, self.handle_quit)
+        self.loop.add_signal_handler(signal.SIGWINCH, self.handle_winch)
+        self.loop.add_signal_handler(signal.SIGUSR1, self.handle_usr1)
+        self.loop.add_signal_handler(signal.SIGABRT, self.handle_abort)
 
-class _wrp:
+        # Don't let SIGTERM and SIGUSR1 disturb active requests
+        # by interrupting system calls
+        signal.siginterrupt(signal.SIGTERM, False)
+        signal.siginterrupt(signal.SIGUSR1, False)
 
-    def __init__(self, proto, meth, tracking, add=True):
-        self._proto = proto
-        self._id = id(proto)
-        self._meth = meth
-        self._tracking = tracking
-        self._add = add
+    def handle_quit(self, sig, frame):
+        self.alive = False
 
-    def __call__(self, *args):
-        if self._add:
-            self._tracking[self._id] = self._proto
-        elif self._id in self._tracking:
-            del self._tracking[self._id]
-
-        conn = self._meth(*args)
-        return conn
+    def handle_abort(self, sig, frame):
+        self.alive = False
+        self.exit_code = 1
