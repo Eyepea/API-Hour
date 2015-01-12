1ebe91d32ddd590968c76d2197325e364a762514
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-01-12 22:00:25 +0100

    ujson is now optional in aiohttp.web plugin

--- api_hour/plugins/aiohttp/__init__.py
+++ api_hour/plugins/aiohttp/__init__.py
@@ -1,12 +1,16 @@
 from aiohttp.web import Response
-import ujson
+
+try:
+    import ujson as json
+except ImportError:
+    import json
 
 class JSON(Response):
     """Serialize response to JSON with aiohttp.web"""
 
     def __init__(self, json, status=200,
                  reason=None, headers=None):
-        body = ujson.dumps(json).encode('utf-8')
+        body = json.dumps(json).encode('utf-8')
 
         super().__init__(body=body, status=status, reason=reason,
                          headers=headers, content_type='application/json')
